124ab1ffd64058252e4521d55484c12915cf26d9
itotallyrock <itotallyrock@users.noreply.github.com>
2016-08-27 19:24:04 -0500

    Troubleshoot travis

--- test/src/classes/song.js
+++ test/src/classes/song.js
@@ -1,5 +1,7 @@
 const path = require('path');
 const {expect} = require('chai');
+console.log(path.join(__dirname.toString().replace('\\test\\', '\\'), 'song'));
+console.log("Directory", fs.readdirSync(path.join(__dirname.toString().replace('\\test\\', '\\'), 'song')));
 const Song = require(path.join(__dirname.toString().replace('\\test\\', '\\'), 'song'));
 
 let defaultSong = process.env.TEST_SONG;
@@ -8,7 +10,7 @@ console.log('DEFAULT SONG', process.env.TEST_SONG);
 
 describe('Songs', function() {
     it('Has a constructor', function(done) {
-        new Song(path.join(__dirname, '../../../public/music/' + decodeURI(defaultSong))).then(() => {
+        new Song(path.join(__dirname, '../../../public/music/', decodeURI(defaultSong))).then(() => {
             done();
         }).catch((err) => {
             done(err);
@@ -16,7 +18,7 @@ describe('Songs', function() {
         expect(Song).to.be.a('function', 'Not a function.');
     });
     it('Should return an object', function(done) {
-        new Song(path.join(__dirname, '../../../public/music/' + decodeURI(defaultSong))).then((song) => {
+        new Song(path.join(__dirname, '../../../public/music/', decodeURI(defaultSong))).then((song) => {
             expect(song).to.be.an('object');
             done();
         }).catch((err) => {
